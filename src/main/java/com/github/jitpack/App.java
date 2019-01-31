6f6b44194da8cd48dfcf290bce12ebd00ddb1891
Prathamesh-10 <40928644+Prathamesh-10@users.noreply.github.com>
2019-01-31 19:28:02 +0530

    Update App.java

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main(String[] args)
     {
         System.out.println(new App().greet("world"));
+        System.out.println("Changes made")
     }
 
     public String greet(String name) {
